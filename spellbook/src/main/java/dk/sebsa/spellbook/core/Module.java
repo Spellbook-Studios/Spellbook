50461c7ed4d2a13bcd419da199ffd659555e281d
Sebastian Snoer <sebs@sebsa.dk>
2023-03-09 17:31:54 +0100

    Here

---
+++ spellbook/src/main/java/dk/sebsa/spellbook/core/Module.java
@@ -0,0 +1,2 @@
+package dk.sebsa.spellbook.core;public interface Module {
+}
---
+++ spellbook/src/main/java/dk/sebsa/spellbook/core/events/Event.java
@@ -0,0 +1,9 @@
+package dk.sebsa.spellbook.core;
+
+/**
+ * A representation of an event occurring inside Spellbook
+ * @author sebsn
+ * @since 0.0.1
+ */
+public class Event {
+}
